b1773b6eda3e572e00a0f4431252258ec439b31c
marcellafarias <marcella.costa@quintoandar.com.br>
2021-07-26 02:27:36 -0300

    Finish Put Test

--- cypress/integration/services/tests/PutUser.spec.js
+++ cypress/integration/services/tests/PutUser.spec.js
@@ -5,7 +5,20 @@ import * as PostUser from '../../../requests/PostUser.request';
 
 describe('Atualiza usuário', () => {
 
-    it('Atualiza o nome do usuário pelo id', () => {
+    it('Cria e Atualiza o nome do usuário pelo id', () => {
+        PostUser.createUser().then((responseCreateUser) => {
+            PutUser.updateUser(responseCreateUser.body.data.id).should((responseUpdateUser) => {
+                expect(responseUpdateUser.status).to.eq(200);
+                expect(responseUpdateUser.body.code).to.eq(200);
+                GetUsers.onlyUser(responseCreateUser.body.data.id).should((responseOnlyUser) => {
+                    expect(responseOnlyUser.status).to.eq(200);
+                    expect(responseOnlyUser.body.code).to.eq(200)
+                })
+            })
+        })
+    });
+
+    it('Atualiza o nome do primeiro usuário pelo id', () => {
         GetUsers.allUsers().then((responseAllUsers) => {
             PutUser.updateUser(responseAllUsers.body.data[0].id).should((responseUpdateUser) => {
                 expect(responseUpdateUser.status).to.eq(200);
@@ -13,12 +26,5 @@ describe('Atualiza usuário', () => {
             })
         })
     })
-    it('Cria e Atualiza o nome do usuário pelo id', () => {
-        PostUser.createUser().then((responseCreateUser) => {
-            PutUser.updateUser(responseCreateUser.body.data.id).should((responseUpdateUser) => {
-                expect(responseUpdateUser.status).to.eq(200);
-                expect(responseUpdateUser.body.code).to.eq(200);
-            })
-        })
-    });
+
 });
