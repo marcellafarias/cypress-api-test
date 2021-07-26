f8b7a4e18cae462b90ffd4600d29e714a7c75ce1
marcellafarias <marcella.costa@quintoandar.com.br>
2021-07-26 04:54:37 -0300

    Organize delete class

--- cypress/integration/services/tests/DeleteUser.spec.js
+++ cypress/integration/services/tests/DeleteUser.spec.js
@@ -4,15 +4,6 @@ import * as PostUser from '../../../requests/PostUser.request';
 
 describe('Exclui usuário', () => {
 
-    it('Exclui usuário pelo id', () => {
-        GetUsers.allUsers().then((responseAllUsers) => {
-            cy.log(responseAllUsers)
-            DeleteUser.deleteUser(responseAllUsers.body.data[0].id).should((responseDeleteUser) => {
-                expect(responseDeleteUser.status).to.eq(200);
-                expect(responseDeleteUser.body.code).to.eq(204);
-            })
-        })
-    })
     it('Cria e Exclui usuário pelo id', () => {
         PostUser.createUser().then((responseCreateUser) => {
             DeleteUser.deleteUser(responseCreateUser.body.data.id).should((responseDeleteUser) => {
@@ -21,4 +12,14 @@ describe('Exclui usuário', () => {
             })
         })
     });
+
+    it('Exclui o primeiro usuário pelo id', () => {
+        GetUsers.allUsers().then((responseAllUsers) => {
+            DeleteUser.deleteUser(responseAllUsers.body.data[0].id).should((responseDeleteUser) => {
+                expect(responseDeleteUser.status).to.eq(200);
+                expect(responseDeleteUser.body.code).to.eq(204);
+            })
+        })
+    })
+
 });
