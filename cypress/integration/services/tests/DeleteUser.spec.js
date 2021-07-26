2bc3ae485a47b75e9971b826aa714473bd7e5ce0
marcellafarias <marcella.costa@quintoandar.com.br>
2021-07-26 05:22:44 -0300

    delete test completed

--- cypress/integration/services/tests/DeleteUser.spec.js
+++ cypress/integration/services/tests/DeleteUser.spec.js
@@ -4,22 +4,47 @@ import * as PostUser from '../../../requests/PostUser.request';
 
 describe('Exclui usuário', () => {
 
-    it('Cria e Exclui usuário pelo id', () => {
+    it('Cria usuário, após o exclui pelo id e verifica se foi retirado da lista', () => {
         PostUser.createUser().then((responseCreateUser) => {
-            DeleteUser.deleteUser(responseCreateUser.body.data.id).should((responseDeleteUser) => {
+            const userId = responseCreateUser.body.data.id;
+            DeleteUser.deleteUser(userId).should((responseDeleteUser) => {
                 expect(responseDeleteUser.status).to.eq(200);
                 expect(responseDeleteUser.body.code).to.eq(204);
+                GetUsers.allUsers().then((response) => {
+                    var lastPage = response.body.meta.pagination.pages
+                    cy.log('Capturado o número total de páginas')
+                    GetUsers.inPage(lastPage).should((newResp) => {
+                        var hasId = false
+                        newResp.body.data.forEach(element => {
+                            console.log(`Visitando elemento de id=${element.id} e comparando com id=${userId}`)
+                            if (element.id == userId) {
+                                hasId = true
+                            }
+                        })
+                        expect(hasId).to.be.false
+                    })
+                })
+            })
+        });
+    });
+
+    it('Exclui o primeiro usuário pelo id e verifica na primeira página se ainda existe o id', () => {
+        GetUsers.allUsers().then((responseAllUsers) => {
+            const userId = responseAllUsers.body.data[0].id;
+            DeleteUser.deleteUser(userId).should((responseDeleteUser) => {
+                expect(responseDeleteUser.status).to.eq(200);
+                expect(responseDeleteUser.body.code).to.eq(204);
+                GetUsers.allUsers().then((response) => {
+                    var hasId = false
+                    response.body.data.forEach(element => {
+                        console.log(`Visitando elemento de id=${element.id} e comparando com id=${userId}`)
+                        if (element.id == userId) {
+                            hasId = true
+                        }
+                    })
+                    expect(hasId).to.be.false
+                })
             })
         })
     });
-
-    it('Exclui o primeiro usuário pelo id', () => {
-        GetUsers.allUsers().then((responseAllUsers) => {
-            DeleteUser.deleteUser(responseAllUsers.body.data[0].id).should((responseDeleteUser) => {
-                expect(responseDeleteUser.status).to.eq(200);
-                expect(responseDeleteUser.body.code).to.eq(204);
-            })
-        })
-    })
-
 });
